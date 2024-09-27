9b0e4bf8f3d76e20bf9985735ebf3dbb7c93109a
haaandolo <ehslee96@gmail.com>
2024-09-28 08:21:37 +1200

    feat: added create chirps handler

--- readiness.go
+++ readiness.go
@@ -2,8 +2,8 @@ package main
 
 import "net/http"
 
-func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits.Store(0)
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits reset to 0"))
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
