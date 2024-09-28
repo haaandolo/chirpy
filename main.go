ad9ea88b2e01720f7e6752b621545608cff552d0
haaandolo <ehslee96@gmail.com>
2024-09-28 17:58:58 +1200

    feat: added webhook endpoint

--- main.go
+++ main.go
@@ -58,6 +58,8 @@ func main() {
 	mux.HandleFunc("POST /api/refresh", apiCfg.handlerRefresh)
 	mux.HandleFunc("POST /api/revoke", apiCfg.handlerRevoke)
 
+	mux.HandleFunc("POST /api/polka/webhooks", apiCfg.handlerWebhook)
+
 	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
 	mux.HandleFunc("PUT /api/users", apiCfg.handlerUsersUpdate)
 
